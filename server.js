308376d8b4b4029101a81427c6d3190b98d644a7
Shivam <shivamkmr0724@gmail.com>
2026-02-16 15:06:47 +0530

    created basic server

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express();
+const PORT = 3000;
+app.get('/',(req, res) =>{
+    res.send("server is running");
+})
+
+app.listen(PORT, () =>{ console.log(`server is running on port ${PORT}`)})
